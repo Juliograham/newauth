7b8c87b3f4e73cd4756fe50077c39426b8602282
LAPTOP-VFT9RDFR\info <julio.graham@ccmshightech.com>
2018-04-14 11:49:39 -0400

    Init commit

---
+++ application/controllers/User.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends MY_Controller {
+    function __construct() {
+        parent::__construct();
+        $this->load->library('ion_auth');
+    }
+
+    public function index() {
+        $this->load->view('welcome_message');
+    }
+
+    public function login() {
+        $this->form_validation->set_rules('username', 'Username', 'trim|required');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->render('user/login_view');
+        } else {
+            $remember = (bool)$this->input->post('remember');
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+
+            if ($this->ion_auth->login($username, $password, $remember)) {
+                redirect('dashboard');
+            } else {
+                $_SESSION['auth_message'] = $this->ion_auth->errors();
+                $this->session->mark_as_flash('auth_message');
+                redirect('user/login_view');
+            }
+        }
+    }
+
+    public function logout() {
+        $this->ion_auth->logout();
+        redirect('user/login');
+    }
+}
